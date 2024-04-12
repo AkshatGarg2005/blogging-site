508e0b06aaf50a58b82f17f970b235034e892308
Akshat Garg <akshat.23bce10641@vitbhopal.ac.in>
2024-04-12 20:22:17 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Logo from "../imgs/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="container">
-                <div className="logo">Logo</div>
-                <div className="links">Links</div>
+                <div className="logo">
+                    <img src={Logo} alt="" />
+                </div>
+
+                <div className="links">
+                    <Link className="link" to="/?cat=art"><h6>ART</h6></Link>
+                    <Link className="link" to="/?cat=science"><h6>SCIENCE</h6></Link>
+                    <Link className="link" to="/?cat=technology"><h6>TECHNOLOGY</h6></Link>
+                    <Link className="link" to="/?cat=cinema"><h6>CINEMA</h6></Link>
+                    <Link className="link" to="/?cat=design"><h6>DESIGN</h6></Link>
+                    <Link className="link" to="/?cat=food"><h6>FOOD</h6></Link>
+                    
+                </div>
             </div>
         </div>
     )
