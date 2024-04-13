c551f3b4a223480f5d1385899ac0aa8df87bac14
Akshat Garg <akshat.23bce10641@vitbhopal.ac.in>
2024-04-13 22:39:48 +0530

    .

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,28 +30,28 @@ const posts=[
 
 const Home = () => {
     return (
-        <div className="home">
-            <div className="posts">
-                {posts.map(post =>(
-                    <div className='post' key={post.id}>
-                     <div className='img'>
-                     <img src={post.img} alt="" />
-                    </div>
-                    <div className='content'>
-                    <Link className="link" to={`post/${post.id}`}>
-                     <h1>{post.title}</h1>
-                     <p>{post.desc}</p>
-                     <button>Read More</button>
-                     </Link>
-                     </div>
-                     </div>
-                ))
-                }
+      <div className="home">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="img">
+                <img src={post.img} alt="" />
+              </div>
+              <div className="content">
+                <Link className="link" to={`/post/${post.id}`}>
+                  <h1>{post.title}</h1>
+                  <p>{post.desc}</p>
+                  <button>Read More</button>
+                </Link>
+              </div>
             </div>
-
-
+          ))
+          }
         </div>
-    )
-}
+
+        
+      </div>
+    );
+  };
 
 export default Home;
