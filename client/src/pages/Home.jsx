47135c96245d11246c6e39c717578c785b4f4261
Akshat Garg <akshat.23bce10641@vitbhopal.ac.in>
2024-04-21 21:05:22 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import {Link} from "react-router-dom";
-
 const posts=[
     {
         id:1,
         title:"Lorem ipsum dolor sit amet consectetur adipisicing elit",
         desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque eos, incidunt reiciendis dolorum cumque delectus natus iure commodi, asperiores iste tempore beatae modi sunt libero nobis adipisci dignissimos voluptatem. Atque.",
-        img:"https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        img:"https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
         id:2,
         title:"Lorem ipsum dolor sit amet consectetur adipisicing elit",
         desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque eos, incidunt reiciendis dolorum cumque delectus natus iure commodi, asperiores iste tempore beatae modi sunt libero nobis adipisci dignissimos voluptatem. Atque.",
-        img:"https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        img:"https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
         id:3,
         title:"Lorem ipsum dolor sit amet consectetur adipisicing elit",
         desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque eos, incidunt reiciendis dolorum cumque delectus natus iure commodi, asperiores iste tempore beatae modi sunt libero nobis adipisci dignissimos voluptatem. Atque.",
-        img:"https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        img:"https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
         id:4,
@@ -30,7 +29,7 @@ const posts=[
 
 const Home = () => {
     return (
-      <div className="home">
+      <div className="Home">
         <div className="posts">
           {posts.map((post) => (
             <div className="post" key={post.id}>
